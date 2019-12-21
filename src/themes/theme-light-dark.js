61db3c9da196e56fe01b31ccd54a3d1aeae96ac4
Charles-Antoine LEGER <ca@leger.email>
2019-12-21 23:42:42 +0100

    update ldtheme

--- src/themes/theme-light-dark.js
+++ src/themes/theme-light-dark.js
@@ -4,17 +4,23 @@ export const themeLD = [
     backgroundColor: "rgb(242, 242, 247)",
     fontColor: "black",
     blue: "rgb(0, 122, 255)",
+    red: "rgb(255, 45, 85)",
     gray: "rgb(142,142,147)",
     gray2: "rgb(174, 174, 178)",
-    gray4: "rgb(209, 209, 214)"
+    gray4: "rgb(209, 209, 214)",
+    gray5: "rgb(229, 229, 234)",
+    gray6: "rgb(242, 242, 247)"
   },
   {
     theme: "dark",
     backgroundColor: "black",
     fontColor: "white",
     blue: "rgb(0, 122, 255)",
+    red: "rgb(255, 45, 85)",
     gray: "rgb(142,142,147)",
     gray2: "rgb(174, 174, 178)",
-    gray4: "rgb(99, 99, 102)"
+    gray4: "rgb(99, 99, 102)",
+    gray5: "rgb(44 ,44 ,46)",
+    gray6: "rgb(28, 28, 60)"
   }
 ];
