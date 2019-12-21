f0159a6a62d5c6e3ab2eed4524fb8a520bb857eb
Charles-Antoine LEGER <ca@leger.email>
2019-12-21 23:42:12 +0100

    update filterbar (adding view mode switcher)

--- src/components/Documents/FilterBar.js
+++ src/components/Documents/FilterBar.js
@@ -2,13 +2,20 @@ import React from "react";
 import { View } from "react-native";
 
 import Switch from "./Filters.js/Switch";
+import ViewMode from "./Filters.js/ViewMode";
 
 export default FilterBar = props => {
   return (
     <View
-      style={{ flexDirection: "row", justifyContent: "center", marginTop: 15 }}
+      style={{
+        flexDirection: "row",
+        justifyContent: "center",
+        marginTop: 15,
+        alignItems: "center"
+      }}
     >
       <Switch {...props} />
+      <ViewMode {...props} />
     </View>
   );
 };
---
+++ src/components/Documents/Filters.js/ViewMode.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, TouchableOpacity } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faList } from "@fortawesome/free-solid-svg-icons";
+import { screenWidth } from "../../../utils/dimensions";
+
+export default ViewMode = props => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      style={{
+        right: screenWidth - 485,
+        borderRadius: 5,
+        alignItems: "center",
+        justifyContent: "center",
+        height: 25,
+        width: 25,
+        backgroundColor:
+          props.viewMode === "Card"
+            ? props.theme.backgroundColor
+            : props.theme.blue
+      }}
+      onPress={() =>
+        props.setViewMode(prevData => (prevData === "List" ? "Card" : "List"))
+      }
+    >
+      <FontAwesomeIcon
+        size={17}
+        icon={faList}
+        color={
+          props.viewMode === "List"
+            ? props.theme.backgroundColor
+            : props.theme.blue
+        }
+      />
+    </TouchableOpacity>
+  );
+};
