2d6de331149a6323a8c9392e0b59ea9a4f63f6d5
Charles-Antoine LEGER <ca@leger.email>
2019-12-30 17:29:37 +0100

    disable tab bar navigation

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,57 +6,58 @@ import { Ionicons } from "@expo/vector-icons";
 
 import BottomTabBar from "../components/Tabs/bottomTabBar";
 
-import DocumentsStack from "./DocumentsNavigator";
+import DocumentsNavigator from "./DocumentsNavigator";
 import ExamNavigator from "./ExamNavigator";
 import SettingsNavigator from "./SettingsNavigator";
 import AccountNavigator from "./AccountNavigator";
 
 import Library from "../views/Library";
 
-const TabBarNavigator = createBottomTabNavigator(
-  {
-    Library: {
-      screen: Library
-    },
-    Documents: {
-      screen: DocumentsStack
-    }
-  },
+// const TabBarNavigator = createBottomTabNavigator(
+//   {
+//     Library: {
+//       screen: Library
+//     },
+//     Documents: {
+//       screen: DocumentsStack
+//     }
+//   },
 
-  {
-    defaultNavigationOptions: ({ navigation, screenProps }) => ({
-      tabBarIcon: ({ focused, horizontal, tintColor }) => {
-        const { routeName } = navigation.state;
-        routeName === "Library"
-          ? (iconName = `ios-albums`)
-          : routeName === "Documents"
-          ? (iconName = `ios-today`)
-          : (iconName = `ios-search`);
+//   {
+//     defaultNavigationOptions: ({ navigation, screenProps }) => ({
+//       tabBarIcon: ({ focused, horizontal, tintColor }) => {
+//         const { routeName } = navigation.state;
+//         routeName === "Library"
+//           ? (iconName = `ios-albums`)
+//           : routeName === "Documents"
+//           ? (iconName = `ios-today`)
+//           : (iconName = `ios-search`);
 
-        return (
-          <Ionicons
-            name={iconName}
-            size={28}
-            color={tintColor}
-            style={{ marginTop: 10 }}
-          />
-        );
-      }
-    }),
-    initialRouteName: "Documents",
-    tabBarComponent: props => (
-      <BottomTabBar
-        {...props}
-        activeTintColor="rgb(0, 122, 255)"
-        inactiveTintColor="rgb(142, 142, 147)"
-      />
-    )
-  }
-);
+//         return (
+//           <Ionicons
+//             name={iconName}
+//             size={28}
+//             color={tintColor}
+//             style={{ marginTop: 10 }}
+//           />
+//         );
+//       }
+//     }),
+//     initialRouteName: "Documents",
+//     tabBarComponent: props => (
+//       <BottomTabBar
+//         {...props}
+//         activeTintColor="rgb(0, 122, 255)"
+//         inactiveTintColor="rgb(142, 142, 147)"
+//       />
+//     )
+//   }
+// );
 
 const AppNavigator = createStackNavigator(
   {
-    TabBarNavigator: TabBarNavigator,
+    // TabBarNavigator: TabBarNavigator,
+    DocumentsNavigator: DocumentsNavigator,
     SettingsNavigator: SettingsNavigator,
     AccountNavigator: AccountNavigator,
     ExamNavigator: ExamNavigator
